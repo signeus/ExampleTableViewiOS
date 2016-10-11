59828a288dadb526351c2c24092c826cbe35588a
Kevin <kev.ms93@gmail.com>
2016-10-11 13:26:32 +0200

    Improved

--- TableViewExample2/SecondScreenViewController.swift
+++ TableViewExample2/SecondScreenViewController.swift
@@ -12,27 +12,15 @@ class SecondScreenViewController: UIViewController {
 
     var texto : String = ""
     
+    @IBOutlet weak var textLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        print("Hola, \(texto)")
-        // Do any additional setup after loading the view.
-    }
+        textLabel.text = texto    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
